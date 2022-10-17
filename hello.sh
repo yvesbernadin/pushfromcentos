8c154f94cbde21ff4a4a84c09da28915c227aa24
root <root@localhost.localdomain>
2022-10-17 20:37:04 +0000

    1committing

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+    #Author : Yves Bernadin
+    #Date : 11-AUG-2021
+
+## ---------- script that display hello word in the screen -----------------
+
+echo "Hello World !"
+sleep 3
+echo "hello yves how are today"
+sleep 3
+pwd
+sleep 6
+uname -r
+sleep 3
+arch
